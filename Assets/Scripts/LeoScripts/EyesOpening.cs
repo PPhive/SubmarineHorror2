68717d4310b2b74e349596ead25b3282bc9d42b7
Primary <yw3726@nyu.com>
2020-12-21 16:06:52 -0500

    Eyesprites

---
+++ Assets/Scripts/LeoScripts/EyesOpening.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesOpening : MonoBehaviour
+{
+    public float MaxCountdown;
+    public Sprite[] Sprites;
+    public SpriteRenderer MyRenderer;
+
+    public int stage;
+
+    public float timer;
+
+    public bool IsOnUI;
+    
+
+    void Start()
+    {
+        MyRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        stage = Mathf.FloorToInt(timer/(MaxCountdown / (Sprites.Length)));
+        if (stage < Sprites.Length)
+        {
+            MyRenderer.sprite = Sprites[stage];
+        }
+
+        if (IsOnUI && timer < MaxCountdown)
+        {
+
+            MyRenderer.color = new Color(1, 0, 0, timer / MaxCountdown);
+            Debug.Log(MyRenderer.color.a);
+        }
+        else
+        {
+            MyRenderer.color = new Color(1, 0, 0, 1);
+        }
+
+    }
+}
