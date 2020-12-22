3d18de33330e02fa207be8757e72a10b08dee315
Primary <yw3726@nyu.com>
2020-12-21 20:12:19 -0500

    Added Radar and Radar Arrows. For now they only work for babies.

---
+++ Assets/Scripts/LeoScripts/Radar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    public GameObject ArrowPrefab;
+    public static Radar instance;
+    public SphereCollider MyCollider;
+
+    private void Awake()
+    {
+        instance = this;
+        MyCollider = GetComponent<SphereCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Instantiate(ArrowPrefab,other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            BroadcastMessage("Exiting", other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LeoScripts/RadarArrows.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarArrows : MonoBehaviour
+{
+    public GameObject Target;
+    private EnemyBaby TargetScript;
+
+    public GameObject Arrow;
+    private float MaxDistance;
+
+    private void Awake()
+    {
+        Target = transform.parent.gameObject;
+        transform.parent = Radar.instance.transform;
+        transform.localPosition = new Vector3(0, 0, 0);
+        transform.localScale = new Vector3(1, 1, 1);
+        TargetScript = Target.GetComponent<EnemyBaby>();
+        MaxDistance = Vector3.Distance(transform.position, Target.transform.position);
+    }
+
+    void Update()
+    {
+        transform.localPosition = new Vector3(0, 0, 0);
+        transform.LookAt(Target.transform);
+        if (!TargetScript.isActiveAndEnabled)
+        {
+            Destroy(this.gameObject);
+        }
+
+        //Arrow Scaling with distance;
+        Arrow.transform.localScale = new Vector3(2, 2, 2) * (1 - (Vector3.Distance(transform.position, Target.transform.position)/MaxDistance));
+    }
+
+    void Exiting(GameObject ExitingBaby)
+    {
+        if (ExitingBaby == Target)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
